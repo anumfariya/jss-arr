cc7546900fb3cfc42a7b017fedd30b7762ca540f
anumfariya <sultan78601234@gmail.com>
2020-03-02 18:06:43 +0530

    origin master

---
+++ script.js
@@ -0,0 +1,40 @@
+// var name='john';
+
+// function first() {
+//     var a='hello!';
+//     second();
+//     var x=a+name;
+//     console.log(x);
+// }
+// function second(){
+//     var b='Hi!';
+//     third();
+//     var y=b+name;
+//     console.log(y);
+// }
+// function third(){
+//     var c='Hey!';
+//     var z=c+name;
+//     console.log(z);
+// }
+// first();
+
+// console.log(a);
+// var a=10;
+// var teacher="anum";
+// function sam(){
+//     teacher="daniya";
+//     topic="foody";
+//     console.log("welcome !",topic);
+// }
+// sam();
+
+var arr = [1,2,3,4,5];
+console.log("arrays elements are:")
+function elements(){
+    for(var i=0;i<=arr.length;i++)
+        console.log(arr[i]+ "");
+}
+elements();
+  console.log("elements");
+  
